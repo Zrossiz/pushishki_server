cdd48f1270d9285bde4501ea832a0161d417d62c
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-11-30 14:25:52 +0300

    fix: order of  slug in brand

--- prisma/migrations/20231130112515_/migration.sql
+++ prisma/migrations/20231130112515_/migration.sql
@@ -100,8 +100,8 @@ CREATE TABLE "reviews" (
 CREATE TABLE "categories" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
     "image" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
