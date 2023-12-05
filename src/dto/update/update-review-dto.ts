00109db714307ed17def8524b13be07309b1cfda
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-05 21:22:50 +0300

    feat: validation for updateReviewDto

--- src/dto/update/update-review-dto.ts
+++ src/dto/update/update-review-dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class UpdateReviewDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   rating: number;
 }
