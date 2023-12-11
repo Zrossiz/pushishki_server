be8893d75f0c4bb1792d2a6a6fc04a88b5f51dab
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-11 16:15:53 +0300

    fix: added props for basket module

--- src/basket/basket.module.ts
+++ src/basket/basket.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { BasketController } from './basket.controller';
+import { BasketService } from './basket.service';
 
-@Module({})
+@Module({
+  controllers: [BasketController],
+  providers: [BasketService, PrismaService],
+  exports: [BasketService],
+})
 export class BasketModule {}
