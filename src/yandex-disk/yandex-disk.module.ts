ef24850d138a4c4de56f03f14d52a8482211be2a
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-18 10:55:02 +0300

    feat: added config yandex-disk module

--- src/yandex-disk/yandex-disk.module.ts
+++ src/yandex-disk/yandex-disk.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { YandexDiskService } from './yandex-disk.service';
+import { YandexDiskController } from './yandex-disk.controller';
 
-@Module({})
+@Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
+  controllers: [YandexDiskController],
+  providers: [YandexDiskService],
+})
 export class YandexDiskModule {}
