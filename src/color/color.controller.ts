9460c4294745599d019cf615d08848f9e1aed859
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-28 14:18:28 +0300

    feat: added create color route

--- src/color/color.controller.ts
+++ src/color/color.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ColorService } from './color.service';
+import { CreateColorDto } from './dto/create-color.dto';
 
 @Controller('color')
-export class ColorController {}
+export class ColorController {
+  constructor(private readonly colorService: ColorService) {}
+
+  @Post('')
+  async create(@Body() createColorDto: CreateColorDto) {
+    return await this.colorService.create(createColorDto);
+  }
+}
--- src/color/color.service.ts
+++ src/color/color.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateColorDto } from './dto/create-color.dto';
+import { Color } from '@prisma/client';
 
 @Injectable()
-export class ColorService {}
+export class ColorService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(
+    createColorDto: CreateColorDto,
+  ): Promise<Color | { message: string }> {
+    try {
+      const color: Color = await this.prismaService.color.create({
+        data: createColorDto,
+      });
+
+      return color;
+    } catch (err) {
+      console.log(err);
+      if (`${err.status}`.startsWith('4')) {
+        throw new BadRequestException(err.message);
+      }
+      throw new InternalServerErrorException('Ошибка сервера');
+    }
+  }
+}
