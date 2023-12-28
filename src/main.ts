3acfe0b3cc9abaaf672adf39f4afe5b71581a4dd
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-28 21:51:47 +0300

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Pushishki API')
+    .setDescription('Документация для api Pushishki')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT).then(() => {
     console.log(`Server started on port: ${process.env.PORT}`);
   });
