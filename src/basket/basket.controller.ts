1ae79124eaaf7782f060455707f0c8c114a1ad6c
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-29 14:44:02 +0300

    feat: added swagger types for basket

--- src/basket/basket.controller.ts
+++ src/basket/basket.controller.ts
@@ -1,18 +1,20 @@
 import { Body, Controller, Param, Post, Get } from '@nestjs/common';
-import { CreateBasketDto } from 'src/basket/dto/create-basket.dto';
+import { BasketDto, CreateBasketDto } from 'src/basket/dto/create-basket.dto';
 import { BasketService } from 'src/basket/basket.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Basket')
 @Controller('basket')
 export class BasketController {
   constructor(private readonly basketService: BasketService) {}
 
+  @ApiBody({ type: BasketDto })
   @Post('')
   async create(@Body() createBasketDto: CreateBasketDto) {
     return await this.basketService.create(createBasketDto);
   }
 
+  @ApiParam({ name: 'id', type: 'string', description: 'Order Id', example: 1 })
   @Get(':id')
   async getBasketsByOrderId(@Param('id') orderId: string) {
     return await this.basketService.getBasketsByOrderId(+orderId);
--- src/basket/dto/create-basket.dto.ts
+++ src/basket/dto/create-basket.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsArray,
@@ -15,37 +16,46 @@ export class CreateBasketDto {
 }
 
 export class BasketDto {
+  @ApiProperty({ example: '1', description: 'Id продукта' })
   @IsNotEmpty()
   @IsNumber()
   productId: number;
 
+  @ApiProperty({ example: '1', description: 'Id варианта продукта' })
   @IsNotEmpty()
   @IsNumber()
   variantId: number;
 
+  @ApiProperty({ example: '1', description: 'Id заказа' })
   @IsNumber()
   orderId: number;
 
+  @ApiProperty({ example: 'Иван', description: 'Имя заказчика' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 'Иванов', description: 'Фамилия заказчика' })
   @IsNotEmpty()
   @IsString()
   lastname: string;
 
+  @ApiProperty({ example: 'Иванович', description: 'Отчество заказчика' })
   @IsNotEmpty()
   @IsString()
   secondname: string;
 
+  @ApiProperty({ example: '+79996305009', description: 'Телефон заказчика' })
   @IsNotEmpty()
   @IsString()
   phone: string;
 
+  @ApiProperty({ example: 10, description: 'Количество товара данной позиции' })
   @IsNotEmpty()
   @IsNumber()
   count: number;
 
+  @ApiProperty({ example: '10000', description: 'Стоимость данной позиции' })
   @IsNotEmpty()
   @IsNumber()
   price: number;
