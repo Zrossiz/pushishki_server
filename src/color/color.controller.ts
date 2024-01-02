44559fd3721db141cb95ecfa42f2d14804b3607c
Egor <96371791+Zrossiz@users.noreply.github.com>
2024-01-02 21:38:20 +0300

    feat: added pipes and swagger types for color

--- src/color/color.controller.ts
+++ src/color/color.controller.ts
@@ -1,18 +1,35 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ColorService } from './color.service';
 import { CreateColorDto } from './dto/create-color.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Color')
 @Controller('color')
 export class ColorController {
   constructor(private readonly colorService: ColorService) {}
 
+  @ApiOperation({ summary: 'Создание цвета' })
   @Post('')
+  @UsePipes(ValidationPipe)
   async create(@Body() createColorDto: CreateColorDto) {
     return await this.colorService.create(createColorDto);
   }
 
+  @ApiOperation({ summary: 'Удаление цвета' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'Id цвета',
+    example: '1',
+  })
   @Delete(':id')
   async delete(@Param('id') colorId: string) {
     return await this.colorService.delete(+colorId);
--- src/color/dto/create-color.dto.ts
+++ src/color/dto/create-color.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateColorDto {
-  @IsNotEmpty()
-  @IsString()
+  @ApiProperty({ example: 'Красный' })
+  @IsNotEmpty({ message: 'Название цвета не может быть пустым значеним' })
+  @IsString({ message: 'Название цвета должно быть строкой' })
   color: string;
 }
