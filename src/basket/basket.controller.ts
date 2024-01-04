d42f6aa07a06d8e69c109252158f699dd5a7cd9d
Egor <96371791+Zrossiz@users.noreply.github.com>
2024-01-04 13:39:58 +0300

    feat: added desc for create basket route

--- src/basket/basket.controller.ts
+++ src/basket/basket.controller.ts
@@ -16,7 +16,23 @@ import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 export class BasketController {
   constructor(private readonly basketService: BasketService) {}
 
-  @ApiOperation({ summary: 'Создание корзины' })
+  @ApiOperation({
+    summary: 'Создание корзины',
+    description: `Пример создания корзины:<br />
+    {
+      &nbsp;"objects":<br />
+      &nbsp;&nbsp;[<br />
+      &nbsp;&nbsp;&nbsp;{<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;"productId": 2,<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;"variantId": 2,<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;"orderId": 5,<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;"count": 1,<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;"price": 1<br />
+    &nbsp;&nbsp;&nbsp;}<br />
+    &nbsp;&nbsp;]<br />
+    }<br />
+    Данный эндпоинт принимает массив объектов`,
+  })
   @ApiBody({ type: BasketDto })
   @Post('')
   @UsePipes(ValidationPipe)
