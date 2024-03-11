b299d76342e9bf2414016b27759c87c0660970b5
Zrossiz <soon.pluxury@mail.ru>
2024-03-11 22:17:36 +0300

    feat: added address field for order

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
 
 export class CreateOrderDto {
   @ApiProperty({ example: 'Иван' })
@@ -12,11 +12,6 @@ export class CreateOrderDto {
   @IsString({ message: 'Фамилия должна быть строкой' })
   lastname: string;
 
-  @ApiProperty({ example: 'Иванович' })
-  @IsNotEmpty({ message: 'Отчество не может быть пустым значением' })
-  @IsString({ message: 'Отчество должно быть строкой' })
-  secondname: string;
-
   @ApiProperty({ example: '+79998887766' })
   @IsNotEmpty({ message: 'Номер не может быть пустым значением' })
   @IsString({ message: 'Номер телефона должен быть строкой' })
@@ -24,6 +19,11 @@ export class CreateOrderDto {
     message: 'Номер телефона должен начинаться с +7 или 8',
   })
   phone: string;
+
+  @ApiProperty({ example: 'г. Москва, Алма-Атинска д8' })
+  @IsOptional()
+  @IsString({ message: 'Адрес должен быть строкой' })
+  address: string;
 
   @ApiProperty({ example: '20000' })
   @IsNotEmpty({ message: 'Стоимость не может быть пустым значением' })
--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -12,16 +12,16 @@ export class UpdateOrderDto {
   @IsString({ message: 'Фамилия должна быть строкой' })
   lastname: string;
 
-  @ApiProperty({ example: 'Иванович' })
-  @IsOptional()
-  @IsString({ message: 'Отчество должно быть строкой' })
-  secondname: string;
-
   @ApiProperty({ example: '+79998887766' })
   @IsOptional()
   @IsString({ message: 'Номер телефона должен быть строкой' })
   @IsPhoneNumber('RU', { message: 'Неверный формат номера телефона' })
   phone: string;
+
+  @ApiProperty({ example: 'г. Москва, Алма-Атинска д8' })
+  @IsOptional()
+  @IsString({ message: 'Адрес должен быть строкой' })
+  address: string;
 
   @ApiProperty({ example: '20000' })
   @IsOptional()
