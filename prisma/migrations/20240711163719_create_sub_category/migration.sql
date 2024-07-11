2f7e6c77d3e5439729d90276818ee50a615c0e4b
Zrossiz <soon.pluxury@mail.ru>
2024-07-11 19:37:37 +0300

    feat: created sub category model

---
+++ prisma/migrations/20240711163719_create_sub_category/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN     "sub_category_id" INTEGER;
+
+-- CreateTable
+CREATE TABLE "sub_category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "category_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sub_category_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sub_category" ADD CONSTRAINT "sub_category_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
